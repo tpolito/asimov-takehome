7993f6476527a0c8f34b758515c33d19a429af8d
TPolito <0613.tyler@gmail.com>
2022-02-05 14:11:51 -0600

    Add color mode switching

--- app/__tests__/Navbar.test.tsx
+++ app/__tests__/Navbar.test.tsx
@@ -1,4 +1,4 @@
-import { render, waitFor } from '@testing-library/react';
+import { render, waitFor, fireEvent } from '@testing-library/react';
 import { Navbar } from '../components';
 
 describe('Navbar', () => {
@@ -6,5 +6,21 @@ describe('Navbar', () => {
     const { getByText, getByLabelText } = render(<Navbar />);
     expect(getByText(/Takehome/i)).toBeInTheDocument();
     expect(getByLabelText(/Asimov Logo/i)).toBeInTheDocument();
+  });
+  test('Should render color mode button', () => {
+    const { getByRole } = render(<Navbar />);
+    expect(getByRole('button')).toBeInTheDocument();
+  });
+  test('Should change color mode when toggle button is preseed', () => {
+    const { getByLabelText, getByRole, rerender } = render(<Navbar />);
+    const button = getByRole('button');
+    expect(button).toBeInTheDocument();
+    expect(getByLabelText(/Sun icon/i)).toBeInTheDocument();
+    fireEvent.click(button);
+    rerender(<Navbar />);
+
+    waitFor(() => {
+      expect(getByLabelText(/Moon icon/i)).toBeInTheDocument();
+    });
   });
 });
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,16 +1,26 @@
-import { Box, Flex, Text, useColorModeValue } from '@chakra-ui/react';
+import { Box, Button, Flex, Text, useColorMode } from '@chakra-ui/react';
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 import Logo from '../public/asimov_logo.svg';
 
 export const Navbar = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
-    <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
-      <Flex h={16} alignItems={'center'} justifyContent={'center'}>
+    <Box bg={colorMode === 'light' ? 'gray.100' : 'gray.900'} px={4}>
+      <Flex h={16} alignItems="center" justifyContent="space-between">
         <Box spacing={9} h={8} display="flex" flexDir="row">
-          <Logo aria-label={'Asimov Logo'} />
+          <Logo aria-label="Asimov Logo" />
           <Text as="span" fontSize="2xl" ml={2}>
             Takehome
           </Text>
         </Box>
+        <Button onClick={toggleColorMode} aria-label="Color mode toggle">
+          {colorMode === 'light' ? (
+            <MoonIcon aria-label="Moon icon" />
+          ) : (
+            <SunIcon aria-label="Sun icon" />
+          )}
+        </Button>
       </Flex>
     </Box>
   );
